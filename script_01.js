cd019802c1cfaff0bd39367b286f81c13170815c
Marcus Beringer <marcusberinger-comcave@web.de>
2022-11-10 08:52:37 +0100

    Anfangh

--- script_01.js
+++ script_01.js
@@ -7,17 +7,17 @@ const ageJohn = 35;
 const ageMark = 30;
 
 // Deklaration
-let isJohnOlder, isJohnEqual;
+let isJohnOlder, isJohnEqual; 
 
 // Test | Logische Aussage
 isJohnOlder = (ageJohn > ageMark);
 isJohnEqual = (ageJohn == ageMark);
 
 // Ausgabe
-// console.log("ageJohn: " + ageJohn);
-// console.log("ageMark: " + ageMark);
-// console.log("isJohnOlder: " + isJohnOlder);
-// console.log("isJohnEqual: " + isJohnEqual);
-// console.log("------------------");
+console.log("ageJohn: " + ageJohn);
+console.log("ageMark: " + ageMark);
+console.log("isJohnOlder: " + isJohnOlder);
+console.log("isJohnEqual: " + isJohnEqual);
+console.log("------------------");
 
 //Anfang
